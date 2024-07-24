6bad4455a225ac5d1281854fdf32507e26f07813
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-07-24 11:58:21 +0630

    feat: common query

---
+++ DotNet8WebApi.MiniKpaySample/Shared/CommonQuery.cs
@@ -0,0 +1,24 @@
+﻿namespace DotNet8WebApi.MiniKpaySample.Shared;
+
+public static class CommonQuery
+{
+    public static string IsExistCustomerCode { get; } =
+        @"select * from Tbl_Customer with (nolock) where CustomerCode = @CustomerCode";
+
+    public static string TransactionHistoryByCustomerCode { get; } =
+        @"select CTH.* from [dbo].[Tbl_CustomerTransactionHistory] CTH
+                                                    inner join Tbl_Customer C on CTH.FromMobileNo = C.MobileNo
+                                                    where CustomerCode = @CustomerCode";
+
+    public static string IsExistTransactionDate { get; } =
+        @"select * from Tbl_Customer with (nolock) where TransactionDate = @TransactionDate";
+
+    public static string TransactionHistoryByDatetime { get; } =
+        @"select CTH.* from [dbo].[Tbl_CustomerTransactionHistory] CTH
+                                                     inner join Tbl_Customer C on CTH.FromMobileNo = C.MobileNo
+                                                     where TransactionDate = @TransactionDate";
+
+    public static string InsertTransactionHistory { get; } =
+        @"INSERT INTO Tbl_CustomerTransactionHistory (FromMobileNo, ToMobileNo, Amount, TransactionDate)
+                                                         VALUES (@FromMobileNo, @ToMobileNo, @Amount, @TransactionDate)";
+}
