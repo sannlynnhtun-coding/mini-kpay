3eb281129462d3368620e72ee3488bf369f9c178
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-07-24 11:58:57 +0630

    feat : config

---
+++ DotNet8WebApi.MiniKpaySample/GlobalUsings.cs
@@ -0,0 +1,12 @@
+﻿// Global using directives
+
+global using System.Data;
+global using System.Xml.XPath;
+global using Dapper;
+global using Microsoft.AspNetCore.Mvc;
+global using DotNet8WebApi.MiniKpaySample.Features.TransactionHistory;
+global using DotNet8WebApi.MiniKpaySample.Models;
+global using DotNet8WebApi.MiniKpaySample.Models.TransactionHistory;
+global using DotNet8WebApi.MiniKpaySample.Models.Transfer;
+global using DotNet8WebApi.MiniKpaySample.Shared;
+global using Microsoft.Data.SqlClient;
---
+++ DotNet8WebApi.MiniKpaySample/Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+string connectionString = builder.Configuration.GetConnectionString("DbConnection")!;
+builder.Services.AddScoped<IDbConnection>(n => new SqlConnection(connectionString));
+
+builder.Services.AddScoped<TransactionHistoryRepository>();
+builder.Services.AddScoped<TransactionHistoryService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
